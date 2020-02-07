1626368a3aaf2b9b225a5bb02691e69a7c4298dc
Josh Bowden <josbow@gmail.com>
2020-02-07 17:03:52 -0600

    chore(tests): tell `make` to stop printing when it `cd`s

--- build-tests.sh
+++ build-tests.sh
@@ -4,5 +4,5 @@ mkdir -p build
 
 for dir in $(ls -d tests/*); do
 	echo "Building $dir"
-	make -f Makefile.tests build_test M=$dir -j
+	make --no-print-directory -f Makefile.tests build_test M=$dir -j
 done
